fde21c6a9a138f04ce6656e05d9fad24ca915262
Zero <zero.griffin@gmail.com>
2018-07-10 15:34:01 +0200

    Scrape items

---
+++ Pyro/Source/Scraper/ScraperConstants.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+struct ScraperConstants {
+    static let prefixCount:String = "data-count=\""
+    static let prefixDate:String = "\" data-date=\""
+    static let dateLenght:Int = 10
+}
