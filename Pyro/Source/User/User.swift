c01f852eefb5b65a1d157d4a187a937e076c8f23
Zero <zero.griffin@gmail.com>
2018-07-12 07:44:22 +0200

    Add identifier to user struct

--- Pyro/Source/User/User.swift
+++ Pyro/Source/User/User.swift
@@ -1,11 +1,13 @@
 import Foundation
 
 public struct User:Codable {
+    public var identifier:String
     public var name:String
     public var url:String
     public var stats:Stats
     
     public init() {
+        self.identifier = String()
         self.name = String()
         self.url = String()
         self.stats = Stats()
