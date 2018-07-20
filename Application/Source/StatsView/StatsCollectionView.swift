109b0bd90cd99ce19aec2a3ccdb9a364989a6e93
Zero <zero.griffin@gmail.com>
2018-07-20 09:04:02 +0200

    Make years responsive to scrolling

--- Application/Source/StatsView/StatsCollectionView.swift
+++ Application/Source/StatsView/StatsCollectionView.swift
@@ -4,8 +4,16 @@ class StatsCollectionView:StatsParentView,
 UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     var items:[StatsItem]
     var collection:YearsView! { get { return self.content.metrics.years } }
+    private var trackingScroll:Bool
+    private var centreIndex:IndexPath? {
+        get {
+            let centre:CGPoint = CGPoint(x:self.collection.bounds.width / 2.0 + self.collection.contentOffset.x, y:0)
+            return self.collection.indexPathForItem(at:centre)
+        }
+    }
     
     required init() {
+        self.trackingScroll = false
         self.items = []
         super.init()
     }
@@ -30,6 +38,16 @@ UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFl
         self.navigationController?.setToolbarHidden(false, animated:false)
     }
     
+    func scrollViewWillBeginDragging(_:UIScrollView) {
+        self.trackingScroll = true
+    }
+    
+    func scrollViewDidScroll(_ view:UIScrollView) {
+        if self.trackingScroll {
+            self.selectCentre()
+        }
+    }
+    
     func collectionView(_:UICollectionView, layout:UICollectionViewLayout, insetForSectionAt:Int) -> UIEdgeInsets {
         let margin:CGFloat = (self.collection.bounds.width - YearsCellView.width) / 2.0
         return UIEdgeInsets(top:0, left:margin, bottom:0, right:margin)
@@ -43,6 +61,7 @@ UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFl
     }
     
     func collectionView(_:UICollectionView, didSelectItemAt index:IndexPath) {
+        self.trackingScroll = false
         self.collection.scrollToItem(at:index, at:UICollectionView.ScrollPosition.centeredHorizontally, animated:true)
         self.presenter.select(item:self.items[index.item])
     }
@@ -60,6 +79,12 @@ UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFl
         self.collection.selectItem(at:IndexPath(item:self.items.count - 1, section:0), animated:true,
                                     scrollPosition:UICollectionView.ScrollPosition.centeredHorizontally)
         self.presenter.select(item:self.items.last!)
+    }
+    
+    private func selectCentre() {
+        guard let indexPath:IndexPath = self.centreIndex else { return }
+        self.collection.selectItem(at:indexPath, animated:false, scrollPosition:UICollectionView.ScrollPosition())
+        self.presenter.select(item:self.items[indexPath.item])
     }
 }
 
