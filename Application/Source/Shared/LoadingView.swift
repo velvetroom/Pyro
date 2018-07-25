9ad716653b46a29476c237fdfb752fc0661ab2c5
Zero <zero.griffin@gmail.com>
2018-07-25 16:02:44 +0200

    Update loading view

--- Application/Source/Shared/LoadingView.swift
+++ Application/Source/Shared/LoadingView.swift
@@ -4,32 +4,18 @@ class LoadingView:UIView {
     weak var circle:CAShapeLayer!
     weak var pulse:CAShapeLayer!
     weak var outer:CAShapeLayer!
-    override var tintColor:UIColor! {
-        didSet {
-            self.circle.fillColor = self.tintColor.cgColor
-            self.pulse.fillColor = self.tintColor.cgColor
-        }
-    }
+    override var tintColor:UIColor! { didSet {
+        self.circle.fillColor = self.tintColor.cgColor
+        self.pulse.fillColor = self.tintColor.cgColor
+    } }
     
     init() {
         super.init(frame:CGRect.zero)
-        self.configureView()
-        self.configureCircle()
-        self.configurePulse()
-        self.animatePulse()
-    }
-    
-    required init?(coder:NSCoder) { return nil }
-    override var intrinsicContentSize:CGSize { get { return CGSize(width:Constants.size, height:Constants.size) } }
-    
-    private func configureView() {
         self.translatesAutoresizingMaskIntoConstraints = false
         self.clipsToBounds = true
         self.isUserInteractionEnabled = false
         self.backgroundColor = UIColor.clear
-    }
-    
-    private func configureCircle() {
+        
         let circle:CAShapeLayer = CAShapeLayer()
         circle.lineWidth = 0
         circle.strokeColor = nil
@@ -39,9 +25,7 @@ class LoadingView:UIView {
         circle.frame = CGRect(x:0, y:0, width:Constants.size, height:Constants.size)
         self.circle = circle
         self.layer.addSublayer(circle)
-    }
-    
-    private func configurePulse() {
+        
         let pulse:CAShapeLayer = CAShapeLayer()
         pulse.lineWidth = 0
         pulse.strokeColor = nil
@@ -51,44 +35,45 @@ class LoadingView:UIView {
         pulse.frame = CGRect(x:0, y:0, width:Constants.size, height:Constants.size)
         self.pulse = pulse
         self.layer.addSublayer(pulse)
-    }
-    
-    private func animatePulse() {
+        
         let groupAnimation:CAAnimationGroup = CAAnimationGroup()
         groupAnimation.animations = [self.animateRadiusPulse(), self.animateRadiusFade(), self.animateAlpha()]
-        groupAnimation.duration = Constants.animationGroupDuration
+        groupAnimation.duration = Constants.animationGroup
         groupAnimation.repeatCount = Float.infinity
         groupAnimation.isRemovedOnCompletion = false
         groupAnimation.fillMode = CAMediaTimingFillMode.forwards
         self.pulse.add(groupAnimation, forKey:Constants.animationKey)
     }
     
+    required init?(coder:NSCoder) { return nil }
+    override var intrinsicContentSize:CGSize { get { return CGSize(width:Constants.size, height:Constants.size) } }
+    
     private func animateRadiusPulse() -> CAAnimation {
         let animation:CABasicAnimation = CABasicAnimation(keyPath:Constants.radiusKey)
-        animation.duration = Constants.animationPulseDuration
+        animation.duration = Constants.animationPulse
         animation.timingFunction = CAMediaTimingFunction(controlPoints:0.4, 0.0, 0.2, 1.0)
-        animation.fromValue = Constants.pulseStartRadiusMultiplier
-        animation.toValue = Constants.pulseMiddleRadiusMultiplier
+        animation.fromValue = Constants.pulseStartRadius
+        animation.toValue = Constants.pulseMiddleRadius
         return animation
     }
     
     private func animateRadiusFade() -> CAAnimation {
         let animation:CABasicAnimation = CABasicAnimation(keyPath:Constants.radiusKey)
-        animation.duration = Constants.animationFadeDuration
+        animation.duration = Constants.animationFade
         animation.timingFunction = CAMediaTimingFunction(controlPoints:0.4, 0.0, 0.2, 1.0)
-        animation.fromValue = Constants.pulseMiddleRadiusMultiplier
-        animation.toValue = Constants.pulseEndRadiusMultiplier
-        animation.beginTime = Constants.animationPulseDuration
+        animation.fromValue = Constants.pulseMiddleRadius
+        animation.toValue = Constants.pulseEndRadius
+        animation.beginTime = Constants.animationPulse
         return animation
     }
     
     private func animateAlpha() -> CAAnimation {
         let animation:CABasicAnimation = CABasicAnimation(keyPath:Constants.alphaKey)
-        animation.duration = Constants.animationFadeDuration
+        animation.duration = Constants.animationFade
         animation.timingFunction = CAMediaTimingFunction(controlPoints:0.4, 0.0, 0.2, 1.0)
         animation.fromValue = Constants.pulseStartAlpha
         animation.toValue = Constants.pulseEndAlpha
-        animation.beginTime = Constants.animationPulseDuration
+        animation.beginTime = Constants.animationPulse
         return animation
     }
     
@@ -106,12 +91,12 @@ private struct Constants {
     static let animationKey:String = "animation"
     static let size:CGFloat = 100
     static let circleRadius:CGFloat = 15
-    static let pulseStartRadiusMultiplier:CGFloat = 1
-    static let pulseMiddleRadiusMultiplier:CGFloat = 1.2
-    static let pulseEndRadiusMultiplier:CGFloat = 2
+    static let pulseStartRadius:CGFloat = 1
+    static let pulseMiddleRadius:CGFloat = 1.2
+    static let pulseEndRadius:CGFloat = 2
     static let pulseStartAlpha:CGFloat = 0.18
     static let pulseEndAlpha:CGFloat = 0
-    static let animationPulseDuration:TimeInterval = 0.3
-    static let animationFadeDuration:TimeInterval = 1.2
-    static let animationGroupDuration:TimeInterval = 1.5
+    static let animationPulse:TimeInterval = 0.3
+    static let animationFade:TimeInterval = 1.2
+    static let animationGroup:TimeInterval = 1.5
 }
